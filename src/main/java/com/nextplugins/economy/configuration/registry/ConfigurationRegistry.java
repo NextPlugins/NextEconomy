4d2975c1243bfd494c4f0d288995f2cef90cc585
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-02-12 16:34:20 -0300

    feat(configuration): file's configuration created.

--- src/main/java/com/nextplugins/economy/configuration/registry/ConfigurationRegistry.java
+++ src/main/java/com/nextplugins/economy/configuration/registry/ConfigurationRegistry.java
@@ -3,6 +3,8 @@ package com.nextplugins.economy.configuration.registry;
 import com.henryfabio.minecraft.configinjector.bukkit.injector.BukkitConfigurationInjector;
 import com.nextplugins.economy.NextEconomy;
 import com.nextplugins.economy.configuration.GeneralConfiguration;
+import com.nextplugins.economy.configuration.MessageValue;
+import com.nextplugins.economy.configuration.RankingConfiguration;
 import lombok.Data;
 
 @Data(staticConstructor = "of")
@@ -13,8 +15,16 @@ public final class ConfigurationRegistry {
     public void register() {
         BukkitConfigurationInjector configurationInjector = new BukkitConfigurationInjector(plugin);
 
+        configurationInjector.saveDefaultConfiguration(
+                plugin,
+                "messages.yml",
+                "ranking.yml"
+        );
+
         configurationInjector.injectConfiguration(
-                GeneralConfiguration.instance()
+                GeneralConfiguration.instance(),
+                MessageValue.instance(),
+                RankingConfiguration.instance()
         );
     }
 
