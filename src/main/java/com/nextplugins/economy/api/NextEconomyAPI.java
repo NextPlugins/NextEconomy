e083cf1d6f85895923c82328fb7c6cb6a1373fee
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-02-12 15:36:47 -0300

    feat(api): NextEconomyAPI pre-created.

---
+++ src/main/java/com/nextplugins/economy/api/NextEconomyAPI.java
@@ -0,0 +1,14 @@
+package com.nextplugins.economy.api;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class NextEconomyAPI {
+
+    @Getter private static final NextEconomyAPI instance = new NextEconomyAPI();
+
+    // todo: next economy's api
+
+}
