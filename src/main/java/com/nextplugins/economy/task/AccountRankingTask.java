c153a478d8853b430216fdf81f623727a0645317
Gabriel Santos <gabrielbarretos2000@outlook.com>
2021-02-18 22:49:03 -0300

    fix(core): some plugins didn't recognize

--- src/main/java/com/nextplugins/economy/task/AccountRankingTask.java
+++ src/main/java/com/nextplugins/economy/task/AccountRankingTask.java
@@ -1,13 +1,10 @@
 package com.nextplugins.economy.task;
 
-import com.nextplugins.economy.api.event.operations.EconomyRankingUpdateEvent;
 import com.nextplugins.economy.api.model.Account;
 import com.nextplugins.economy.dao.AccountDAO;
 import com.nextplugins.economy.storage.RankingStorage;
 import lombok.RequiredArgsConstructor;
-import org.bukkit.Bukkit;
 
-import java.time.Instant;
 import java.util.Set;
 
 @RequiredArgsConstructor
@@ -22,13 +19,8 @@ public final class AccountRankingTask implements Runnable {
         Set<Account> accounts = accountDAO.selectAll("ORDER BY balance DESC LIMIT 10");
 
         if (!accounts.isEmpty()) {
-
             rankingStorage.getRankingAccounts().clear();
             accounts.forEach(rankingStorage::addAccount);
-
-            EconomyRankingUpdateEvent rankingUpdateEvent = new EconomyRankingUpdateEvent(accounts, Instant.now(), true);
-            Bukkit.getPluginManager().callEvent(rankingUpdateEvent);
-
         }
 
     }
