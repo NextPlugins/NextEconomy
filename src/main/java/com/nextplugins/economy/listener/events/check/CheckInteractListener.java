b962e4585c2feb03470f835eeaaf94f1f60ec3e4
Yuhtin <rio.daviduarte@gmail.com>
2021-05-10 00:48:38 -0300

    fix: simplify check system

--- src/main/java/com/nextplugins/economy/listener/events/check/CheckInteractListener.java
+++ src/main/java/com/nextplugins/economy/listener/events/check/CheckInteractListener.java
@@ -7,6 +7,7 @@ import com.nextplugins.economy.configuration.MessageValue;
 import com.nextplugins.economy.util.NumberUtils;
 import de.tr7zw.changeme.nbtapi.NBTItem;
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -21,37 +22,30 @@ public final class CheckInteractListener implements Listener {
 
     @EventHandler
     public void onCheckInteract(PlayerInteractEvent event) {
-        final Player player = event.getPlayer();
-        final ItemStack item = event.getItem();
+
+        val player = event.getPlayer();
+        val item = event.getItem();
 
         if (item == null || item.getType() == Material.AIR) return;
 
-        final NBTItem nbtItem = new NBTItem(item);
+        val nbtItem = new NBTItem(item);
         if (!nbtItem.hasKey("value")) return;
 
-        final double checkValue = nbtItem.getDouble("value");
+        val checkValue = nbtItem.getDouble("value");
+        val totalValue = checkValue * item.getAmount();
+        val account = accountStorage.findOnlineAccount(player);
 
-        final Account account = accountStorage.findOnlineAccount(player);
+        account.createTransaction(
+                "Cheque",
+                totalValue,
+                TransactionType.DEPOSIT
+        );
 
-        double totalValue = 0;
-        int totalChecks = 0;
-
-        for (int i = 0; i < item.getAmount(); i++) {
-            account.createTransaction(
-                    null,
-                    checkValue,
-                    TransactionType.DEPOSIT
-            );
-
-            totalValue = totalValue + checkValue;
-
-            player.setItemInHand(null);
-            totalChecks++;
-        }
+        player.setItemInHand(null);
 
         player.sendMessage(
                 MessageValue.get(MessageValue::checkUsed)
-                        .replace("$checkAmount", NumberUtils.format(totalChecks))
+                        .replace("$checkAmount", NumberUtils.format(item.getAmount()))
                         .replace("$checkValue", NumberUtils.format(checkValue))
                         .replace("$checkTotalValue", NumberUtils.format(totalValue))
         );
