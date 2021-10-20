8ba4e9048d34633c0cc205beecfc046f0b8a6913
uRyanxD <familiarodrigues123ro@gmail.com>
2021-10-20 19:20:39 -0300

    fix: some things

--- src/main/java/com/nextplugins/economy/listener/events/others/RankingEntityListener.java
+++ src/main/java/com/nextplugins/economy/listener/events/others/RankingEntityListener.java
@@ -4,6 +4,7 @@ import com.nextplugins.economy.ranking.types.ArmorStandRunnable;
 import com.nextplugins.economy.util.TypeUtil;
 import lombok.val;
 import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -20,14 +21,12 @@ public class RankingEntityListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onDeath(EntityDamageByEntityEvent event) {
-        if (!event.getEntity().hasMetadata("nexteconomy")) return;
+        if (event.getEntityType() != EntityType.ARMOR_STAND || !event.getEntity().hasMetadata("nexteconomy")) return;
 
         if (event.getDamager() instanceof Player) {
-
             val damager = (Player) event.getDamager();
             val itemInHand = damager.getItemInHand();
-            if (itemInHand != null && itemInHand.getType() == TypeUtil.swapLegacy("GOLDEN_SWORD", "GOLD_SWORD")) return;
-
+            if (itemInHand.getType() == TypeUtil.swapLegacy("GOLDEN_SWORD", "GOLD_SWORD")) return;
         }
 
         event.setCancelled(true);
@@ -35,14 +34,14 @@ public class RankingEntityListener implements Listener {
 
     @EventHandler
     public void onKill(EntityDeathEvent event) {
-        if (!event.getEntity().hasMetadata("nexteconomy")) return;
+        if (event.getEntityType() != EntityType.ARMOR_STAND || !event.getEntity().hasMetadata("nexteconomy")) return;
         ArmorStandRunnable.STANDS.remove((ArmorStand) event.getEntity());
     }
 
     @EventHandler
     public void onInteract(PlayerInteractAtEntityEvent event) {
         val entity = event.getRightClicked();
-        if (!entity.hasMetadata("nexteconomy")) return;
+        if (entity.getType() != EntityType.ARMOR_STAND || !entity.hasMetadata("nexteconomy")) return;
 
         event.getPlayer().performCommand("money top");
         event.setCancelled(true);
